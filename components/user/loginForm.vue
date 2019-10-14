ab7deed1c7df5a8abc13a91e07afbdf272f06b9f
webshitian <1047221702@qq.com>
2019-10-14 17:03:38 +0800

    9-首页-请求登录接口成功

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -57,19 +57,28 @@ export default {
     },
     methods: {
         // 提交登录
-        handleLoginSubmit(){
+         handleLoginSubmit(){
         //    console.log(this.form)
-            this.$refs.form.validate(valid => {
+            this.$refs.form.validate(async valid => {
                 // valid是表单验证的结果
                 // console.log(valid)
                 if(valid){
                     //提交登录接口
-                    
-                }
+                   var res = await this.$axios({
+                                url:"/accounts/login",
+                                method:"POST",
+                                data:this.form
+                            })
+                            if(res.status === 200){
+                                this.$message.success("登录成功");
+
+                                //this.$router.push('/')
+                            }
+                        }
                 
-            })
+                })
+             }
         }
-    }
 }
 </script>
 
