41f54a278d6341f378aea12c807c639099d4427c
webshitian <1047221702@qq.com>
2019-10-15 15:35:41 +0800

    11-首页-把用户信息展示在头部

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -81,7 +81,7 @@ export default {
 
                                 //通过调用mutation下的方法来修改state的值,commit方法调用mutation的方法
                                 //非常类似于$emit
-                                this.$store.commit("user/setUsername",data.user.nickname)
+                                this.$store.commit("user/setUserInfo",data)
 
                             }
                         }
--- store/user.js
+++ store/user.js
@@ -1,14 +1,15 @@
 //固定的写法，暴露出state
 export const state = {
-    username:"webshitian"
+    userInfo:{}
 }
 
 //存放的是同步修改state的方法
 export const mutations = {
     //mutation下的值必须是一个函数，函数中会有一个固定的参数state。第二个参数是用户调用方法时候传进来的
     //非常类似于$emit
-    setUsername(state,data){
-        state.username = data;
+    //data是登录成功后返回的对象数据
+    setUserInfo(state,data){
+        state.userInfo = data;
     }
 }
 
