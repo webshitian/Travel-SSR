144ad1d573e485845e5fb3694322fccf04fd98d2
webshitian <1047221702@qq.com>
2019-10-18 16:59:09 +0800

    21-机票-获取搜索建议城市

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -55,7 +55,7 @@ export default {
 
 <style scoped lang="less">
 .container{
-    background:url(http://157.122.54.189:9095/assets/images/th03.jfif) center 0;
+    background:url(/th03.jfif) center 0;
     height: 700px;
     min-width:1000px;
 
